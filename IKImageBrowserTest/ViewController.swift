ea068026917fd5bee86402931f2e30d2341c03c2
Vitek Tajzich <v.tajzich@gmai.com>
2016-02-10 17:44:01 +0100

    Initial Commit

---
+++ IKImageBrowserTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  IKImageBrowserTest
+//
+//  Created by Vitezslav Tajzich on 10/02/16.
+//  Copyright © 2016 Vitezslav Tajzich. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
